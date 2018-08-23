1a78c8145632e33201ceef908a92c2b173c0de11
Khoutsaneh Kettavong <laekettavong@Khoutsanehs-iMac.local>
2018-08-22 23:30:39 -0400

    Finished scaffolding

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
 
 // view engine setup
+var swig = require('swig');
+app.engine('html', swig.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+//app.set('view engine', 'hjs');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -37,5 +41,19 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+// ////for mongodb version 3.0 and up
+// let MongoClient = require('mongodb').MongoClient;
+// MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true } , function(err, client){
+//    if(err) throw err;
+   
+//    let db = client.db('pizzia');
+//    db.collection('person').find().toArray(function(err, result){
+//      if(err) throw err;
+//      console.log(result);
+//      client.close();
+//    });
+// })
 
 module.exports = app;
